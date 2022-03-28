165182b955d19798f8d3fd92cb4876eb916f0780
Trường Minh <minhsip2510@gmail.com>
2022-03-28 13:58:24 +0700

    change pdf viewer to download pdf

--- src/utils/components/PDFViewer/index.tsx
+++ src/utils/components/PDFViewer/index.tsx
@@ -5,7 +5,6 @@ import {
   AccordionItem,
   AccordionPanel,
   Box,
-  Center,
   Collapse,
   Drawer,
   DrawerBody,
@@ -20,11 +19,14 @@ import {
   Text,
   Tooltip,
   useDisclosure,
+  Center,
   useColorModeValue,
 } from "@chakra-ui/react";
+import NotFound from "../../../pages/404";
 import WebViewer, { Core, UI } from "@pdftron/webviewer";
 import { FC, useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Button } from "react-day-picker";
+import { useNavigate, useParams, Navigate } from "react-router-dom";
 import { Card, CircularProgressInderterminate, BigContainer } from "../";
 import { useGetFileQuery } from "../../../features/fileUpload";
 import { usePlagiarismCheckMutation } from "../../../features/plagiarismCheck";
@@ -48,185 +50,195 @@ const PDFViewer: FC = (props) => {
   const { toast } = useAppState();
   const file = useGetFileQuery(fileId);
 
-  const handleSearchPlag = useCallback(
-    async (documentViewer: Core.DocumentViewer) => {
-      try {
-        const text = documentViewer.getSelectedText();
-        onOpen();
-        const result = await checkPlag({ text }).unwrap();
-        // const newArr = [...plagChecked];
-        // newArr.push({ text, data: result });
-        const newPlagChecked = [...plagChecked, { text, data: result }];
-        setPlagChecked(newPlagChecked);
-        toast({
-          duration: 60000,
-          status: "success",
-          title: "Kiểm tra đạo văn",
-          description: "Hoàn tất kiểm tra đạo văn",
-        });
-      } catch (error: any) {
-        toast({
-          duration: 60000,
-          status: "error",
-          title: "Kiểm tra đạo văn",
-          description: error.data.message,
-        });
-      }
-    },
-    [plagChecked]
+  return !!file.data ? (
+    <Center>
+      <Link
+        href={file.data?.downloadUri}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Tải xuống
+      </Link>
+    </Center>
+  ) : (
+    <NotFound />
   );
 
-  const ribbonHeader = useMemo(() => {
-    return <h2>{file.data?.title}</h2>;
-  }, [file.data]);
+  // const handleSearchPlag = useCallback(
+  //   async (documentViewer: Core.DocumentViewer) => {
+  //     try {
+  //       const text = documentViewer.getSelectedText();
+  //       onOpen();
+  //       const result = await checkPlag({ text }).unwrap();
+  //       // const newArr = [...plagChecked];
+  //       // newArr.push({ text, data: result });
+  //       const newPlagChecked = [...plagChecked, { text, data: result }];
+  //       setPlagChecked(newPlagChecked);
+  //       toast({
+  //         duration: 60000,
+  //         status: "success",
+  //         title: "Kiểm tra đạo văn",
+  //         description: "Hoàn tất kiểm tra đạo văn",
+  //       });
+  //     } catch (error: any) {
+  //       toast({
+  //         duration: 60000,
+  //         status: "error",
+  //         title: "Kiểm tra đạo văn",
+  //         description: error.data.message,
+  //       });
+  //     }
+  //   },
+  //   [plagChecked]
+  // );
 
-  useEffect(() => {
-    setIsLoading(true);
-    // if (file.data?.downloadUri && !file.isLoading) {
-    WebViewer(
-      {
-        path: "pdftron",
-        initialDoc: file.data?.downloadUri,
-        css: "pdftron/styles/index.css",
-        disableLogs: true,
-        disabledElements: [
-          // "ribbons",
-          "toolsHeader",
-          "toggleNotesButton",
-          "viewControlsButton",
-          "contextMenuPopup",
-          "zoomOutButton",
-          "zoomInButton",
-          "selectToolButton",
-          "panToolButton",
-          "printButton",
-          "downloadButton",
-        ],
-      },
-      viewerBox.current as HTMLDivElement
-    )
-      .then((instance) => {
-        const { documentViewer } = instance.Core;
-        instance.UI.setLanguage("vi");
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   // if (file.data?.downloadUri && !file.isLoading) {
+  //   WebViewer(
+  //     {
+  //       path: "pdftron",
+  //       initialDoc: file.data?.downloadUri,
+  //       css: "pdftron/styles/index.css",
+  //       disableLogs: true,
+  //       disabledElements: [
+  //         // "ribbons",
+  //         "toolsHeader",
+  //         "toggleNotesButton",
+  //         "viewControlsButton",
+  //         "contextMenuPopup",
+  //         "zoomOutButton",
+  //         "zoomInButton",
+  //         "selectToolButton",
+  //         "panToolButton",
+  //         "printButton",
+  //         "downloadButton",
+  //       ],
+  //     },
+  //     viewerBox.current as HTMLDivElement
+  //   )
+  //     .then((instance) => {
+  //       const { documentViewer } = instance.Core;
+  //       instance.UI.setLanguage("vi");
 
-        if (authenticated && role < Role.users) {
-          instance.UI.setHeaderItems((header) => {
-            const leftPanel = header.get("leftPanelButton");
-            leftPanel.insertBefore({
-              type: "actionButton",
-              img: "/view/favicon.svg",
-              onClick: () => navigate("/"),
-            });
-            const searchButton = header.get("searchButton");
-            searchButton.insertBefore({
-              type: "actionButton",
-              img: "/view/download.svg",
-              onClick: () =>
-                file.data?.downloadUri &&
-                window.open(file.data?.downloadUri, "_blank")?.focus(),
-            });
-            searchButton.insertBefore({
-              type: "customElement",
-              render: () => <h2>{file.data?.title}</h2>,
-            });
-            header.push({
-              type: "actionButton",
-              img: "/view/search-para.svg",
-              onClick: onOpen,
-            });
-          });
+  //       if (authenticated && role < Role.users) {
+  //         instance.UI.setHeaderItems((header) => {
+  //           const leftPanel = header.get("leftPanelButton");
+  //           leftPanel.insertBefore({
+  //             type: "actionButton",
+  //             img: "/view/favicon.svg",
+  //             onClick: () => navigate("/"),
+  //           });
+  //           const searchButton = header.get("searchButton");
+  //           searchButton.insertBefore({
+  //             type: "actionButton",
+  //             img: "/view/download.svg",
+  //             onClick: () =>
+  //               file.data?.downloadUri &&
+  //               window.open(file.data?.downloadUri, "_blank")?.focus(),
+  //           });
+  //           searchButton.insertBefore({
+  //             type: "customElement",
+  //             render: () => <h2>{file.data?.title}</h2>,
+  //           });
+  //           header.push({
+  //             type: "actionButton",
+  //             img: "/view/search-para.svg",
+  //             onClick: onOpen,
+  //           });
+  //         });
 
-          instance.UI.textPopup.update([
-            {
-              type: "actionButton",
-              img: "/view/search-para.svg",
-              onClick: () => handleSearchPlag(documentViewer),
-            },
-          ]);
-        } else {
-          instance.UI.textPopup.update([]);
-        }
+  //         instance.UI.textPopup.update([
+  //           {
+  //             type: "actionButton",
+  //             img: "/view/search-para.svg",
+  //             onClick: () => handleSearchPlag(documentViewer),
+  //           },
+  //         ]);
+  //       } else {
+  //         instance.UI.textPopup.update([]);
+  //       }
 
-        instance.UI.setTheme(
-          (localStorage.getItem("chakra-ui-color-mode") ||
-            "dark") as unknown as UI.Theme
-        );
+  //       instance.UI.setTheme(
+  //         (localStorage.getItem("chakra-ui-color-mode") ||
+  //           "dark") as unknown as UI.Theme
+  //       );
 
-        documentViewer.addEventListener(
-          "documentLoaded",
-          () => {
-            setIsLoading(false);
-            console.log("document loaded");
-          },
-          { once: true }
-        );
-      })
-      .catch((e) => console.error({ status: "error", title: e.message }));
-    // }
-  }, []);
+  //       documentViewer.addEventListener(
+  //         "documentLoaded",
+  //         () => {
+  //           setIsLoading(false);
+  //           console.log("document loaded");
+  //         },
+  //         { once: true }
+  //       );
+  //     })
+  //     .catch((e) => console.error({ status: "error", title: e.message }));
+  //   // }
+  // }, []);
 
-  useEffect(() => console.log({ plagChecked }), [plagChecked]);
+  // useEffect(() => console.log({ plagChecked }), [plagChecked]);
 
-  if (file.isError || !file.data) {
-    return (
-      <BigContainer>
-        <Box textAlign="center" py={10} px={6}>
-          <Box display="inline-block">
-            <Flex
-              flexDirection="column"
-              justifyContent="center"
-              alignItems="center"
-              bg={"red.500"}
-              rounded={"50px"}
-              w={"55px"}
-              h={"55px"}
-              textAlign="center"
-            >
-              <CloseIcon boxSize={"20px"} color={"white"} />
-            </Flex>
-          </Box>
-          <Heading as="h2" size="xl" mt={6} mb={2}>
-            Đã có lỗi xảy ra
-          </Heading>
-          <Text color={"gray.500"}>{file.error as any}</Text>
-        </Box>
-      </BigContainer>
-    );
-  }
+  // if (file.isError || !file.data) {
+  //   return (
+  //     <BigContainer>
+  //       <Box textAlign="center" py={10} px={6}>
+  //         <Box display="inline-block">
+  //           <Flex
+  //             flexDirection="column"
+  //             justifyContent="center"
+  //             alignItems="center"
+  //             bg={"red.500"}
+  //             rounded={"50px"}
+  //             w={"55px"}
+  //             h={"55px"}
+  //             textAlign="center"
+  //           >
+  //             <CloseIcon boxSize={"20px"} color={"white"} />
+  //           </Flex>
+  //         </Box>
+  //         <Heading as="h2" size="xl" mt={6} mb={2}>
+  //           Đã có lỗi xảy ra
+  //         </Heading>
+  //         <Text color={"gray.500"}>{file.error as any}</Text>
+  //       </Box>
+  //     </BigContainer>
+  //   );
+  // }
 
-  return (
-    <BigContainer p={0} maxW="full">
-      {isLoading && <Skeleton isLoaded={!isLoading}></Skeleton>}
-      <Box h="100vh" ref={viewerBox} />
-      {authenticated && role < Role.users && (
-        <Drawer
-          portalProps={{
-            appendToParentPortal: true,
-            containerRef: viewerBox,
-          }}
-          size="md"
-          isOpen={isOpen}
-          placement="right"
-          onClose={onClose}
-          blockScrollOnMount={false}
-          trapFocus={false}
-        >
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Kiểm tra đạo văn</DrawerHeader>
-            <DrawerBody overflowY="scroll" fontSize="sm">
-              {checkPlagData.isLoading && <CircularProgressInderterminate />}
-              <Accordion allowToggle>
-                {plagChecked.reverse().map((plag, pIndex) => (
-                  <PlagAccordionItem plag={plag} key={`plag-${pIndex}`} />
-                ))}
-              </Accordion>
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
-      )}
-    </BigContainer>
-  );
+  // return (
+  //   <BigContainer p={0} maxW="full">
+  //     {isLoading && <Skeleton isLoaded={!isLoading}></Skeleton>}
+  //     <Box h="100vh" ref={viewerBox} />
+  //     {authenticated && role < Role.users && (
+  //       <Drawer
+  //         portalProps={{
+  //           appendToParentPortal: true,
+  //           containerRef: viewerBox,
+  //         }}
+  //         size="md"
+  //         isOpen={isOpen}
+  //         placement="right"
+  //         onClose={onClose}
+  //         blockScrollOnMount={false}
+  //         trapFocus={false}
+  //       >
+  //         <DrawerContent>
+  //           <DrawerCloseButton />
+  //           <DrawerHeader>Kiểm tra đạo văn</DrawerHeader>
+  //           <DrawerBody overflowY="scroll" fontSize="sm">
+  //             {checkPlagData.isLoading && <CircularProgressInderterminate />}
+  //             <Accordion allowToggle>
+  //               {plagChecked.reverse().map((plag, pIndex) => (
+  //                 <PlagAccordionItem plag={plag} key={`plag-${pIndex}`} />
+  //               ))}
+  //             </Accordion>
+  //           </DrawerBody>
+  //         </DrawerContent>
+  //       </Drawer>
+  //     )}
+  //   </BigContainer>
+  // );
 };
 
 export default PDFViewer;
